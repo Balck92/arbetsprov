d25324eabf160d6a841be222a56296e5a7f95e44
Carl Johan Balck <carljohanbalck@gmail.com>
2019-08-23 08:03:29 +0200

    Final_last commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Like from "../common/like";
-import ShareFacebookButton from "./sharebutton";
 
 class Card extends Component {
   constructor(props) {
